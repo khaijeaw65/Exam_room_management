97dff34861850cce27908ea1a745550300ef754f
KJ <fieldlegoza@gmail.com>
2020-11-02 02:59:50 +0700

    format file

--- footer.php
+++ footer.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<!-- css files -->
 	<link href="css/bootstrap.css" rel='stylesheet' type='text/css' /><!-- bootstrap css -->
-	
+
 	<link href="css/font-awesome.min.css" rel="stylesheet"><!-- fontawesome css -->
 	<!-- //css files -->
-	
+
 	<!-- google fonts -->
 	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&amp;subset=cyrillic,cyrillic-ext,greek,greek-ext,latin-ext,vietnamese" rel="stylesheet">
 	<link href="//fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700,800&amp;subset=latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Kanit:400,500,600&display=swap&subset=latin-ext,thai,vietnamese" rel="stylesheet">
 	<!-- //google fonts -->
 </head>
+
 <body>
-<!-- footer -->
-<footer>
-	<div class="container py-sm-3">
-		<div class="row footer-grids">
-			<div class="col-lg-3 col-sm-6 mb-lg-0 mb-sm-5 mb-4">
-				<h4 class="mb-sm-4 mb-3">ที่อยู่</h4>
-				<p><span class="fa mr-2 fa-map-marker"></span>มหาวิทยาลัยรังสิต
-					เลขที่ 52/347 หมู่บ้านเมืองเอก ถ.พหลโยธิน  ต.หลักหก อ.เมือง  <span>ปทุมธานี 12000</span></p>
-				<p class="phone py-2"><span class="fa mr-2 fa-phone"></span> 02-791-6000</p>
-				<p><span class="fa mr-2 fa-envelope"></span><a href="mailto:info@rsu.ac.th">info@rsu.ac.th</a></p>
+	<!-- footer -->
+	<footer>
+		<div class="container py-sm-3">
+			<div class="row footer-grids">
+				<div class="col-lg-3 col-sm-6 mb-lg-0 mb-sm-5 mb-4">
+					<h4 class="mb-sm-4 mb-3">ที่อยู่</h4>
+					<p><span class="fa mr-2 fa-map-marker"></span>มหาวิทยาลัยรังสิต
+						เลขที่ 52/347 หมู่บ้านเมืองเอก ถ.พหลโยธิน ต.หลักหก อ.เมือง <span>ปทุมธานี 12000</span></p>
+					<p class="phone py-2"><span class="fa mr-2 fa-phone"></span> 02-791-6000</p>
+					<p><span class="fa mr-2 fa-envelope"></span><a href="mailto:info@rsu.ac.th">info@rsu.ac.th</a></p>
+				</div>
 			</div>
 		</div>
-	</div>
-</footer>
-<!-- //footer -->
+	</footer>
+	<!-- //footer -->
 </body>
+
 </html>
